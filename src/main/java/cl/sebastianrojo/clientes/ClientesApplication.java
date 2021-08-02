18346bdfcad6cbd92e1d23817fa7cbc28d11318b
Sebastian Rojo <sebastianrojocollao@gmail.com>
2021-08-01 21:29:17 -0400

    Agrega clase main

--- src/main/java/cl/sebastianrojo/clientes/ClientesApplication.java
+++ src/main/java/cl/sebastianrojo/clientes/ClientesApplication.java
@@ -1,9 +1,11 @@
 package cl.sebastianrojo.clientes;
 
+import cl.sebastianrojo.clientes.view.MainMenu;
+
 public class ClientesApplication {
 
 	public static void main(String[] args) {
-		
+		new MainMenu();
 	}
 	
 }
